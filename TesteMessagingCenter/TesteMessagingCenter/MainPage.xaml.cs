21b063936853665d7dcdf09da5a7ff720967d34b
Marcelo Pereira Santos <lemarcelo@outlook.com>
2019-10-09 20:36:55 -0300

    Add project files.

---
+++ TesteMessagingCenter/TesteMessagingCenter/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TesteMessagingCenter
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void EnviarMensagem(object sender, EventArgs e)
+        {
+            ResultView destinoMsg = new ResultView();
+            MessagingCenter.Send<ResultView>( destinoMsg, "jose");
+        }
+
+        private void GoPagina2(object sender, EventArgs e)
+        {
+            ResultView Pagina = new ResultView();
+            App.Current.MainPage.Navigation.PushAsync(Pagina);
+            Pagina.Registro();
+        }
+    }
+}
---
+++ TesteMessagingCenter/TesteMessagingCenter/ResultView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TesteMessagingCenter
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ResultView : ContentPage
+    {
+        public ResultView()
+        {
+            InitializeComponent();
+        }
+        public ResultView(bool registro)
+        {
+            InitializeComponent();
+            Registro();
+        }
+
+        private void Voltar(object sender, EventArgs e)
+        {
+            App.Current.MainPage.Navigation.PopAsync();
+        }
+
+        private void ApagarRegistro(object sender, EventArgs e)
+        {
+            MessagingCenter.Unsubscribe<ResultView>(this, "jose");
+        }
+
+        private void RegistrarMensagem(object sender, EventArgs e)
+        {
+            Registro();
+        }
+        //
+        public void Registro()
+        {
+            MessagingCenter.Subscribe<ResultView>(this, "jose", message =>
+            {
+                this.DisplayAlert("asd", "Mensagem Registrada", "asd");
+            });
+        }
+    }
+}
